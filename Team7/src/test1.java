cafb8e7adcd077f54c463a859fb79f551298b9d2
soroush62 <112301194+soroush62@users.noreply.github.com>
2023-04-12 19:55:18 +0300

    Hi

--- Team7/src/test1.java
+++ Team7/src/test1.java
@@ -1,14 +1,10 @@
 
 public class test1 {
-<<<<<<< HEAD
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//Hello worlDD
+		//Hello worlDDD
 
 	}
 
-=======
-// hello world this is a test
->>>>>>> 27b00cbeaad0b5621940f267bccb16b9209e5725
 }
