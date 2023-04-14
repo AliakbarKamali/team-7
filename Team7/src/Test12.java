54b9f7d2e40f1b9cab575c89d437847e10ae85b7
hataheriandani <hosseinali22000@student.hamk.fi>
2023-04-14 22:59:34 +0300

    Why don't show another class in my git's Package Explore?

--- Team7/src/Test12.java
+++ Team7/src/Test12.java
@@ -3,7 +3,8 @@ public class Test12 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-//test12 taheri
+//test12 TAHERI
+		//why don't show another class in my git's Package Explore?
 	}
 
 }
