ed7e1fc39e7f2eb9a4380d5d6c46771574ba0bc5
soroush62 <112301194+soroush62@users.noreply.github.com>
2023-04-20 12:32:41 +0300

    Update Runclass.java
    Added some comments for motors

--- Team7/src/Runclass.java
+++ Team7/src/Runclass.java
@@ -8,7 +8,7 @@ public class Runclassss extends Thread {
 	public static void main(String[] args) {
 		AtomicBoolean robotStop = new AtomicBoolean(false);
 		AtomicBoolean robotrotate = new AtomicBoolean(false);
-		Motors motorsObj = new Motors();
+		Motors motorsObj = new Motors();  //create new object for Motors
 		LineFollow LFObj = new LineFollow(robotStop, robotrotate);  //create new object for LineFollow
 		Obstacle ODObj = new Obstacle(robotStop, robotrotate); //create new object for Obstacle
 		Thread thread1 = new Thread(LFObj);    //  creating a thread object to run in LineFollow class 
