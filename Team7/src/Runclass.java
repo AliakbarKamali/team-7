4248fc696c20376389280bb2c6ea0e04d44612d6
Fatemeh001 <fatemeh22001@student.hamk.fi>
2023-04-20 16:30:58 +0300

    add some commits

--- Team7/src/Runclass.java
+++ Team7/src/Runclass.java
@@ -7,7 +7,7 @@ public class Runclassss extends Thread {
 
 	public static void main(String[] args) {
 		AtomicBoolean robotStop = new AtomicBoolean(false);
-		AtomicBoolean robotrotate = new AtomicBoolean(false);
+		AtomicBoolean robotrotate = new AtomicBoolean(false); //create new object for AtomicBoolean
 		Motors motorsObj = new Motors();  //create new object for Motors
 		LineFollow LFObj = new LineFollow(robotStop, robotrotate);  //create new object for LineFollow
 		Obstacle ODObj = new Obstacle(robotStop, robotrotate); //create new object for Obstacle
